8ba53e518354e1a5c7b6360b7b1e255a528713d4
Dmitrii Gerasimenko <kiddima@gmail.com>
2016-12-03 05:40:05 +0800

    completed user auth API

--- project_name/api.py
+++ project_name/api.py
@@ -17,8 +17,8 @@ import copy
 router = Router(namespace='api')
 
 
-NonFieldError = s.Object(
-    non_field_errors=s.Array(s.String()),
+Errors = s.Object(
+    errors=s.Array(s.String()),
 )
 
 
@@ -32,6 +32,9 @@ UserDef = s.Definition('User', s.Object(
 
 
 class Signin(ApiView):
+    '''
+    User signin
+    '''
     spec = Spec(
         Method.POST,
         s.Object(
@@ -39,7 +42,7 @@ class Signin(ApiView):
             password=s.String(),
         ),
         Response(200, schema=UserDef),
-        Response(400, schema=NonFieldError),
+        Response(400, schema=Errors),
         Response(423),
     )
 
@@ -58,10 +61,13 @@ class Signin(ApiView):
         if user and (not user.email_confirmed or not user.is_active):
             return 423
 
-        return 400, {'non_field_errors': ['Email and/or password not recognized']}
+        return 400, {'errors': ['Email and/or password not recognized']}
 
 
 class Logout(ApiView):
+    '''
+    User Logout
+    '''
     spec = Spec(
         Method.POST,
         s.Empty,
@@ -75,7 +81,7 @@ class Logout(ApiView):
 
 class Signup(ApiView):
     '''
-    Signup user
+    User signup (Register)
     '''
     spec = Spec(
         Method.POST,
@@ -87,8 +93,8 @@ class Signup(ApiView):
             next=s.Optional(s.String()),
         ),
         Response(202, description='user created, need to email confirmation'),
-        Response(301, description='user created, redirect to next page'),
-        Response(400, schema=NonFieldError),
+        Response(301, description='user created, redirect to the next page'),
+        Response(400, schema=Errors),
     )
 
     def create_user(self, **kwargs):
@@ -104,7 +110,7 @@ class Signup(ApiView):
         data['email'] = data['email'].lower()
 
         if User.objects.filter(Q(email=data['email']) | Q(new_email=data['email'])).exists():
-            return 400, {'non_field_errors': ['This email is already in use, please sign in']}
+            return 400, {'errors': ['This email is already in use, please sign in']}
 
 
         if settings.EMAIL_CONFIRMATION:
@@ -119,6 +125,9 @@ class Signup(ApiView):
 
 
 class EmailConfirm(ApiView):
+    '''
+    Confirm user email
+    '''
     spec = Spec(
         Method.GET,
         s.Query(
@@ -126,8 +135,198 @@ class EmailConfirm(ApiView):
             code=s.String(),
             next=s.Optional(s.String()),
         ),
-        Response(204),
+        Response(301, description='redirect to the next page'),
     )
 
     def handle(self, data):
-        return 204
+        code = data['code']
+        user = User.objects.filter(pk=data['id']) \
+            .exclude(email=None, new_email=None) \
+            .filter(Q(email_confirm_code=code) | Q(new_email_confirm_code=code)) \
+            .first()
+
+        if user:
+            if user.email_confirm_code == code:
+                user.email_confirmed = True
+                user.email_confirm_code = None
+                user.is_active = True
+                user.save(update_fields=['email_confirmed', 'email_confirm_code', 'is_active'])
+
+                signals.email_confirmed.send(User, user=user)
+
+                login(self.request, user)
+                signals.signup_completed.send(User, user=user)
+
+            elif user.new_email_confirm_code == code:
+                user.backend = 'django.contrib.auth.backends.ModelBackend'
+                login(self.request, user)
+                signals.signup_completed.send(User, user=user)
+
+                if not User.objects.filter(email=user.new_email).exists():
+                    user.email = user.new_email
+                    user.new_email = None
+                    user.new_email_confirm_code = None
+                    user.email_confirmed = True
+                    user.save(update_fields=['email', 'new_email', 'new_email_confirm_code', 'email_confirmed'])
+
+        next = data['next'] or '/'
+
+        return redirect(next)
+
+
+class ResendEmailConfirm(ApiView):
+    '''
+    Re-send user email confirmation
+    '''
+    spec = Spec(
+        Method.POST,
+        s.Object(
+            email=s.String()
+        ),
+        Response(202),
+    )
+
+    def handle(self, data):
+        email = data['email'].lower()
+        user = User.objects.filter(is_active=True).filter(Q(email=email) | Q(new_email=email)).first()
+        if user:
+            if (email == user.email and user.email_confirm_code):
+                signals.user_email_confirm.send(User, user=user, email=email)
+
+            if (email == user.new_email and user.new_email_confirm_code):
+                signals.user_new_email_confirm.send(User, user=user, email=email)
+
+        return 202
+
+
+class ChangeEmail(ApiView):
+    '''
+    Change user email
+    '''
+    spec = Spec(
+        Method.POST,
+        s.Object(
+            email=s.String(),
+        ),
+        Response(403, description='unauthorized access'),
+        Response(400, schema=Errors),
+        Response(200, schema=s.Object(
+            email=s.String(),
+        ))
+    )
+
+    def handle(self, data):
+        user = getattr(self.request, 'user', None)
+        if not (user or user.is_authenticated):
+            return 403
+
+        email = data['email'].lower()
+
+        if User.objects.exclude(pk=user.pk).filter(Q(email=email) | Q(new_email=email)):
+            return 400, {'errors': ['This email is already in use']}
+
+        if user.email != email:
+            user.email = email
+            user.save(update_fields={'email'})
+
+        if user.email == email:
+            user.new_email = None
+            user.new_email_confirm_code = None
+        else:
+            user.new_email = email
+            user.new_email_confirm_code = random_key()
+
+            signals.user_new_email_confirm.send(User, user=user, email=email)
+
+        user.save(update_fields={'new_email', 'new_email_confirm_code'})
+
+        return 200, {'email': user.email}
+
+
+class ResetPassword(ApiView):
+    '''
+    Reset password confirmation
+    '''
+    spec = Spec(
+        Method.POST,
+        s.Object(
+            email=s.String(),
+        ),
+        Response(202),
+    )
+
+    def handle(self, data):
+        email = data['email'].lower()
+        user = User.objects.filter(email=email, is_active=True).first()
+
+        if user:
+            user.password_reset_code = random_key()
+            user.save(update_fields={'password_reset_code'})
+            signals.password_reset.send(User, user=user)
+
+        return 202
+
+
+class SetPassword(ApiView):
+    '''
+    Set new user password
+    '''
+    spec = Spec(
+        Method.POST,
+        s.Object(
+            id=s.Integer(),
+            code=s.String(),
+            password=s.String(),
+        ),
+        Response(200, schema=UserDef),
+        Response(400, schema=Errors),
+    )
+
+    def handle(self, data):
+        user = User.objects.filter(pk=data['id'], is_active=True, password_reset_code=data['code']).first()
+        if not user:
+            return 400, {'errors': ['Invalid password reset code']}
+
+        user.set_password(data['password'])
+        user.email_confirmed = True
+        user.password_reset_code = None
+        user.save(update_fields={'password', 'email_confirm', 'password_reset_code'})
+
+        login(self.request, user)
+
+        return 200, {
+            'id': user.pk,
+            'email': user.email,
+            'first_name': user.first_name,
+            'short_name': user.get_short_name(),
+        }
+
+
+class ChangePassword(ApiView):
+    '''
+    Change user password
+    '''
+    spec = Spec(
+        Method.POST,
+        s.Object(
+            old_password=s.String(),
+            new_password=s.String(),
+        ),
+        Response(403, description='unauthorized access'),
+        Response(400, schema=Errors),
+        Response(202),
+    )
+
+    def handle(self, data):
+        user = getattr(self.request, 'user', None)
+        if not (user or user.is_authenticated):
+            return 403
+
+        if not user.check_password(data['old_password']):
+            return 400, {'errors': ['Wrong password']}
+
+        user.set_password(data['new_password'])
+        user.save(update_fields={'password'})
+
+        return 202
+
